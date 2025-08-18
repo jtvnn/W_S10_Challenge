99c97b96eef64206c14ffcb3ad96b3a9c71ab62a
Jason Vann <vannjt1@icloud.com>
2025-08-18 02:37:36 -0400

    Fix orderSlice

--- frontend/state/orderSlice.js
+++ frontend/state/orderSlice.js
@@ -1,24 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    order: [],
     filter: 'All',
-    loading: false,
-    error: null,
 };
 // make a slice for order
 const orderSlice = createSlice({
-    name: 'order',
+    name: 'orders',
     initialState,
     reducers: {
-        addOrder: (state, action) => {
-            state.orders.push(action.payload);
-        },
         setFilter: (state, action) => {
             state.filter = action.payload;
         },
     },
 });
 
-export const { addOrder, setFilter } = orderSlice.actions;
+export const { setFilter } = orderSlice.actions;
 export default orderSlice.reducer;
